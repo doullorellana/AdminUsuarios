ddbb0acf0edf0508e45962c4e283bf6da9b8dab5
doullorellana <doullorellana@gmail.com>
2023-12-11 21:25:53 -0600

    Subiendo cambios al repositorio por primera vez

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Usuario;
+use Illuminate\Http\Request;
+
+class UsuarioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return Usuario::all();
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $usuario = new Usuario();
+        $usuario -> nombre = $request -> nombre;
+        $usuario -> apellido = $request -> apellido;
+        $usuario -> correo = $request -> correo;
+        $usuario -> fecha_registro = $request -> fecha_registro;
+        $usuario -> save();
+        return "El usuario se guardó correctamente.";
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Usuario $usuario, $id)
+    {
+        $usuario = Usuario::find($id);
+        return $usuario;
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Usuario $usuario)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Usuario $usuario)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Usuario $usuario)
+    {
+        //
+    }
+}
---
+++ database/factories/UsuarioFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Usuario>
+ */
+class UsuarioFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'nombre' => fake() -> name(),
+            'apellido' => fake() -> lastname(),
+            'correo' => fake() -> email(),
+            'fecha_registro' => fake() -> date(),
+        ];
+    }
+}
